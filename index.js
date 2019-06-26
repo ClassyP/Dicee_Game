8e0dedd43b986288d9167aa695486ef64d060eb9
classyp <jeanphilip@me.com>
2019-06-26 10:21:02 -0700

    added buttonto refesh the page

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+
+
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
